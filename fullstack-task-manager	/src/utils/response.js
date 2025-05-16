8cb757714c0f4e31e5f6a2f22fcc663e968b8d9c
guyunxiang <guyunxiang32@gmail.com>
2025-05-16 19:26:22 -0400

    remove extra folder

--- fullstack-task-manager	/src/utils/response.js
+++
@@ -1,9 +0,0 @@
-function success(message = "Success", data) {
-  return { success: true, message, data };
-}
-
-function error(message = "Error", data, code) {
-  return { success: false, message, data, code };
-}
-
-module.exports = { success, error };
